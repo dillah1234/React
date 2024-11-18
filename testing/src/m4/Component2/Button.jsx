a061113e1c2c8b429a7b803291568c02a3cadef7
dillah1234 <arrafif0722@abudzar.sch.id>
2024-11-18 11:02:02 +0700

    last update

--- testing/src/m4/Component2/Button.jsx
+++ testing/src/m4/Component2/Button.jsx
@@ -1,8 +1,12 @@
-export default function Button({type}) {
+import { Children } from "react";
+
+export default function Button({ children, type }) {
   return (
     <button
       type={type}
       className="bg-yellow-500 hover:bg-yellow-600 text-black font-semibold py-2 px-4 rounded-md w-full"
-    />
+    >
+      {children}
+    </button>
   );
 }
